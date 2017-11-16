d6dc08c444b91be82fe3dd7df2dce9f391e4c0b1
VadimFarutin <vfarut@gmail.com>
2017-11-16 21:50:20 +0300

    Replaced buttons with FAB

--- GPSTracker/app/src/main/java/ru/spbau/farutin_solikov/gpstracker/TrackerActivity.java
+++ GPSTracker/app/src/main/java/ru/spbau/farutin_solikov/gpstracker/TrackerActivity.java
@@ -5,9 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,9 +25,12 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import java.util.ArrayList;
 
 public class TrackerActivity extends DrawerActivity implements OnMapReadyCallback {
+	private static final int ZOOM = 15;
+	
 	private CoordinatesReceiver receiver;
 	private GoogleMap map;
 	private LatLng lastPosition;
+	private ArrayList<Controller.Coordinate> route;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,33 +41,7 @@ public class TrackerActivity extends DrawerActivity implements OnMapReadyCallbac
 				.findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
 		
-		Button start = findViewById(R.id.start_tracker);
-		start.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				map.clear();
-				lastPosition = null;
-				
-				Controller.startCoordinatesService(TrackerActivity.this);
-				
-				receiver = new CoordinatesReceiver();
-				IntentFilter intentSFilter = new IntentFilter("ru.spbau.farutin_solikov.gpstracker.BroadcastCoordinatesAction");
-				registerReceiver(receiver, intentSFilter);
-			}
-		});
-		
-		Button stop = findViewById(R.id.stop_tracker);
-		stop.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Controller.stopCoordinatesService();
-				try {
-					unregisterReceiver(receiver);
-				} catch (IllegalArgumentException ignored) {
-					// no API methods to tell if it is registered at the moment
-				}
-			}
-		});
+		createMenu();
 	}
 	
 	@Override
@@ -73,10 +57,116 @@ public class TrackerActivity extends DrawerActivity implements OnMapReadyCallbac
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
 		map = googleMap;
-		map.moveCamera(CameraUpdateFactory.zoomBy(15));
 	}
 	
-	public class CoordinatesReceiver extends BroadcastReceiver {
+	private void createMenu() {
+		final FloatingActionsMenu menu = findViewById(R.id.menu_tracker);
+		
+		FloatingActionButton start = new FloatingActionButton(this);
+		start.setTitle("Start trip");
+		start.setColorNormalResId(R.color.colorPrimaryDark);
+		start.setColorPressedResId(R.color.colorPrimary);
+		start.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				menu.collapse();
+				
+				map.clear();
+				lastPosition = null;
+				route = new ArrayList<Controller.Coordinate>();
+				
+				Controller.startCoordinatesService(TrackerActivity.this);
+				
+				receiver = new CoordinatesReceiver();
+				IntentFilter intentSFilter = new IntentFilter("ru.spbau.farutin_solikov.gpstracker.BroadcastCoordinatesAction");
+				registerReceiver(receiver, intentSFilter);
+			}
+		});
+		
+		FloatingActionButton stop = new FloatingActionButton(this);
+		stop.setTitle("Stop");
+		stop.setColorNormalResId(R.color.colorPrimaryDark);
+		stop.setColorPressedResId(R.color.colorPrimary);
+		stop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				menu.collapse();
+				
+				Controller.stopCoordinatesService();
+				try {
+					unregisterReceiver(receiver);
+				} catch (IllegalArgumentException ignored) {
+					// no API methods to tell if it is registered at the moment
+				}
+			}
+		});
+		
+		FloatingActionButton save = new FloatingActionButton(this);
+		save.setTitle("Save route");
+		save.setColorNormalResId(R.color.colorPrimaryDark);
+		save.setColorPressedResId(R.color.colorPrimary);
+		save.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				menu.collapse();
+				
+				saveRoute();
+			}
+		});
+		
+		menu.addButton(start);
+		menu.addButton(stop);
+		menu.addButton(save);
+		
+		FloatingActionButton find = findViewById(R.id.find_tracker);
+		find.setColorNormalResId(R.color.colorPrimaryDark);
+		find.setColorPressedResId(R.color.colorPrimary);
+		find.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (lastPosition != null) {
+					map.moveCamera(CameraUpdateFactory.newLatLngZoom(lastPosition, ZOOM));
+				}
+			}
+		});
+	}
+	
+	private void saveRoute() {
+		final Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in);
+		final Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out);
+		
+		// Replace with Dialog
+		final TextInputLayout inputLayout = findViewById(R.id.text_input2);
+		inputLayout.setVisibility(View.VISIBLE);
+		inputLayout.startAnimation(slide_in);
+		
+		final EditText nameInput = findViewById(R.id.route_name2);
+		nameInput.setText("");
+		
+		Button save = findViewById(R.id.save2);
+		save.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				String name = nameInput.getText().toString();
+				if (name.length() != 0) {
+					Controller.sendCoordinates(route, name);
+					inputLayout.startAnimation(slide_out);
+					inputLayout.setVisibility(View.GONE);
+				}
+			}
+		});
+		
+		Button cancel = findViewById(R.id.cancel2);
+		cancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				inputLayout.startAnimation(slide_out);
+				inputLayout.setVisibility(View.GONE);
+			}
+		});
+	}
+	
+	private class CoordinatesReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			Bundle notificationData = intent.getExtras();
@@ -88,17 +178,18 @@ public class TrackerActivity extends DrawerActivity implements OnMapReadyCallbac
 			if (lastPosition == null) {
 				Controller.Coordinate position = coordinates.get(0);
 				lastPosition = new LatLng(position.getLat(), position.getLng());
+				map.moveCamera(CameraUpdateFactory.newLatLngZoom(lastPosition, ZOOM));
 			}
 
 			polylineOptions.add(lastPosition);
 			for (Controller.Coordinate position : coordinates) {
 				polylineOptions.add(new LatLng(position.getLat(), position.getLng()));
+				route.add(position);
 			}
 			
 			Controller.Coordinate position = coordinates.get(coordinates.size() - 1);
 			lastPosition = new LatLng(position.getLat(), position.getLng());
 			
-			map.moveCamera(CameraUpdateFactory.newLatLng(lastPosition));
 			map.addPolyline(polylineOptions);
 		}
 	}
